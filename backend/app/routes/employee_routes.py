fad144f8a5667a45654c1ebcf98b233306c59e39
priyarajak <priyarajakprl@gmail.com>
2026-03-25 21:46:25 +0530

    clean requirements

--- backend/app/routes/employee_routes.py
+++ backend/app/routes/employee_routes.py
@@ -8,20 +8,28 @@ router = APIRouter()
 employees_collection = db["employees"]
 
 @router.post("/employees")
-async def create_employee(employee: EmployeeCreate):
+async def add_employee(employee: EmployeeCreate):
     try:
-        existing = await employees_collection.find_one({"employee_id": employee.employee_id})
-        if existing:
-            raise HTTPException(status_code=409, detail="Employee exists")
+        # Check duplicate
+        existing = await employees_collection.find_one({
+            "employee_id": employee.employee_id
+        })
 
-        await employees_collection.insert_one(employee.model_dump())
+        if existing:
+            raise HTTPException(status_code=400, detail="Employee already exists")
+
+        data = employee.model_dump()
+
+        # ✅ CONVERT DATE
+        data["joining_date"] = str(data["joining_date"])
+
+        await employees_collection.insert_one(data)
 
         return {"message": "Employee added"}
 
     except Exception as e:
         print("ERROR:", e)
         raise HTTPException(status_code=500, detail=str(e))
-
 @router.get("/employees")
 async def get_employees():
     try:
